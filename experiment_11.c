b8078c9bae1974f02ffa40122b579f7c24381e6a
pulkit-cyber <pulkitaryaninfo@gmail.com>
2025-12-31 16:09:27 +0530

    Program to Print 1 to 10 Using Loops

---
+++ experiment_11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    printf("Numbers 1 to 10:\n");
+    for (i = 1; i <= 10; ++i) {
+        printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
