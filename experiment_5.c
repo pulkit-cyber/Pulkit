fb91f2d1448f511e0ad7386cc6f9bc13f256ef8e
pulkit-cyber <pulkitaryaninfo@gmail.com>
2025-12-31 15:55:32 +0530

    Program Demonstrating Data Types and Format Specifiers

---
+++ experiment_5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int integerVar = 10;
+    float floatVar = 10.5;
+    char charVar = 'A';
+    double doubleVar = 20.99;
+
+    printf("Integer: %d\n", integerVar);
+    printf("Float: %.2f\n", floatVar);
+    printf("Character: %c\n", charVar);
+    printf("Double: %lf\n", doubleVar);
+    
+    return 0;
+}
